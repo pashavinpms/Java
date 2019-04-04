a6d96808b0012f96a81ac20acdfa97d3774e1807
pashavinogr <48278027+pashavinogr@users.noreply.github.com>
2019-04-04 08:44:27 +0300

    Update Runner.java

--- practice2/src/Runner.java
+++ practice2/src/Runner.java
@@ -3,7 +3,7 @@ import by.gsu.pmslab.Subject;
 
 public class Runner {
     public static void main(String[] args) {
-        Subject wire=new Subject(Material.STEEL);
+        Subject wire=new Subject(Material.STEEL, 0.03);
         System.out.println(wire);
         wire.setMaterial(Material.COPPER);
         System.out.println("The wire mass is " + wire.getMass()+" kg ");
